a2b608d6556519444b1e4535dec41c2b218e254f
BaruchHLu <baruch.h.lu@gmail.com>
2025-02-26 16:55:36 -0500

    Finish backend ENV setup and start servers

--- packages/backend/server.ts
+++ packages/backend/server.ts
@@ -1,8 +1,13 @@
 // packages/backend/src/server.ts
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3001;
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello from Glory Fellowship backend!');
